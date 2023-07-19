86ad4fa75c4dc7a2ecbfde49dfd04a2bd03a8345
Augustin <augustinlaouar@hotmail.com>
2023-07-19 11:22:51 +0200

    psw bug solved

--- scripts/password_tools.js
+++ scripts/password_tools.js
@@ -1,45 +1,4 @@
 
-export async function storeLogs(url, id, psw) {
-  try{
-    var aesKey = await getAesKey();
-    const encryptedPsw = await encryptWithAES(psw, aesKey);
-    localStorage.setItem("url_"+url, id + ';' + encryptedPsw);
-    addNewUrl(url);
-  }
-  catch(error){
-    console.log(error);
-  }
-
-}
-
-export function storeUrlList(urlList) {
-  // Convertir la liste en une chaîne de caractères
-  const urlListString = urlList.join(';');
-
-  // Stocker la chaîne de caractères dans le localStorage
-  localStorage.setItem('urlList', urlListString);
-}
-
-export function getUrlList() {
-  const urlListString = localStorage.getItem('urlList');
-
-  if (urlListString) {
-    const urlList = urlListString.split(';');
-    return urlList;
-  } else {
-    return [];
-  }
-}
-
-export function addNewUrl(url) {
-  var urlList = getUrlList();
-  // Vérification si l'URL existe déjà dans la liste
-  if (!urlList.includes("url_"+url)) {
-    urlList.push(url);
-    storeUrlList(urlList);
-  }
-}
-
 export async function generateAESKey() {
   const keySize = 256;
   const key = await window.crypto.subtle.generateKey(
@@ -52,6 +11,7 @@ export async function generateAESKey() {
   );
   return key;
 }
+
 
 export async function encryptWithAES(data, key) {
   const encodedData = new TextEncoder().encode(data);
@@ -80,123 +40,124 @@ export async function encryptWithAES(data, key) {
   return encryptedResult;
 }
 
-export async function storeAesKeyEncrypted(aesKey) {
-  const derivedKey = await generateDerivedKey("titi"); // Récupère la clé dérivée
-  const encryptedAesKey = await encryptWithAES(aesKey, derivedKey); // Chiffre la clé AES avec la clé dérivée
 
-  localStorage.setItem('encryptedAesKey', encryptedAesKey);
+export async function decryptWithAESKey(encryptedData, key) {
+  const ivHex = encryptedData.substr(0, 32);
+  const encryptedHex = encryptedData.substr(32);
+
+  const iv = new Uint8Array(ivHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+  const encryptedArray = new Uint8Array(encryptedHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+  const decryptedData = await window.crypto.subtle.decrypt(
+    {
+      name: 'AES-CBC',
+      iv: iv
+    },
+    key,
+    encryptedArray
+  );
+
+  const decryptedString = new TextDecoder().decode(decryptedData);
+  return decryptedString;
+}
+
+export async function encryptBytesWithAes(bytes, key){ //return str
+  const iv = window.crypto.getRandomValues(new Uint8Array(16));
+  const encryptedData = await window.crypto.subtle.encrypt(
+    {
+      name: 'AES-CBC',
+      iv: iv
+    },
+    key,
+    bytes
+  );
+
+  const encryptedArray = new Uint8Array(encryptedData);
+  const encryptedHex = Array.prototype.map
+    .call(encryptedArray, byte => ('00' + byte.toString(16)).slice(-2))
+    .join('');
+
+  const ivHex = Array.prototype.map
+    .call(iv, byte => ('00' + byte.toString(16)).slice(-2))
+    .join('');
+
+  const encryptedResult = ivHex + encryptedHex;
+  return encryptedResult;
 }
 
 
-  export async function decryptWithAESKey(encryptedData, key) {
-    const ivHex = encryptedData.substr(0, 32);
-    const encryptedHex = encryptedData.substr(32);
-  
-    const iv = new Uint8Array(ivHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-    const encryptedArray = new Uint8Array(encryptedHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-  
-    const decryptedData = await window.crypto.subtle.decrypt(
+export async function decryptBytesWithAes(encryptedData, key) { //return bytes
+  const ivHex = encryptedData.substr(0, 32);
+  const encryptedHex = encryptedData.substr(32);
+
+  const iv = new Uint8Array(ivHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+  const encryptedArray = new Uint8Array(encryptedHex.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+  const decryptedData = await window.crypto.subtle.decrypt(
+    {
+      name: 'AES-CBC',
+      iv: iv
+    },
+    key,
+    encryptedArray
+  );
+  const decryptedArray = new Uint8Array(decryptedData);
+  return decryptedArray;
+}
+
+export async function generateDerivedKey(password) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(password);
+  const hashBuffer = await window.crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const first16Bytes = hashArray.slice(0, 16);
+  const keyBytes = new Uint8Array(first16Bytes)
+  const key = await window.crypto.subtle.importKey(
+    "raw", // Format de la clé : bytes bruts
+    keyBytes, // Suite de bytes passée en paramètre
+    { name: "AES-CBC" }, // Algorithme AES-CBC
+    false, // Non-extractable : la clé ne peut pas être exportée
+    ["encrypt", "decrypt"] // Opérations autorisées avec la clé
+  );
+  return key;
+}
+
+export async function storeDerivedKey(derivedKey) {
+  const derivedKeyData = await window.crypto.subtle.exportKey('raw', derivedKey);
+  const derivedKeyString = Array.from(new Uint8Array(derivedKeyData))
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+  localStorage.setItem('derivedKey', derivedKeyString);
+}
+
+export async function getDerivedKey() {
+  const derivedKeyString = localStorage.getItem('derivedKey');
+  if (derivedKeyString) {
+    const keyData = new Uint8Array(derivedKeyString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+    const derivedKey = await window.crypto.subtle.importKey(
+      'raw',
+      keyData,
       {
-        name: 'AES-CBC',
-        iv: iv
+        name: 'AES-CBC'
       },
-      key,
-      encryptedArray
+      false,
+      ['encrypt', 'decrypt']
     );
-  
-    const decryptedString = new TextDecoder().decode(decryptedData);
-    return decryptedString;
+    return derivedKey;
+  } else {
+    return null;
   }
+}
 
 
-  export async function generateDerivedKey(password) {
-    const encoder = new TextEncoder();
-    const data = encoder.encode(password);
-    const hashBuffer = await window.crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const first16Bytes = hashArray.slice(0, 16);
-    const keyBytes = new Uint8Array(first16Bytes)
-    const key = await window.crypto.subtle.importKey(
-      "raw", // Format de la clé : bytes bruts
-      keyBytes, // Suite de bytes passée en paramètre
-      { name: "AES-CBC" }, // Algorithme AES-CBC
-      false, // Non-extractable : la clé ne peut pas être exportée
-      ["encrypt", "decrypt"] // Opérations autorisées avec la clé
-    );
-    return key;
-  }
+export async function storeAesKeyEncrypted(aesKey) {
+  const exportedAesKey = await window.crypto.subtle.exportKey('raw', aesKey);
+  const aesKeyBytes = new Uint8Array(exportedAesKey);
+  const derivedKey = await generateDerivedKey("titi"); // Récupère la clé dérivée
+  const encryptedAesKey = await encryptBytesWithAes(aesKeyBytes, derivedKey); // Chiffre la clé AES avec la clé dérivée
+  localStorage.setItem('encryptedAesKey', encryptedAesKey);
+}
 
-  export async function storeDerivedKey(derivedKey) {
-    const derivedKeyData = await window.crypto.subtle.exportKey('raw', derivedKey);
-    const derivedKeyString = Array.from(new Uint8Array(derivedKeyData))
-      .map(byte => byte.toString(16).padStart(2, '0'))
-      .join('');
-    localStorage.setItem('derivedKey', derivedKeyString);
-  }
-
-  export async function getDerivedKey() {
-    const derivedKeyString = localStorage.getItem('derivedKey');
-    if (derivedKeyString) {
-      const keyData = new Uint8Array(derivedKeyString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
-      const derivedKey = await window.crypto.subtle.importKey(
-        'raw',
-        keyData,
-        {
-          name: 'AES-CBC'
-        },
-        false,
-        ['encrypt', 'decrypt']
-      );
-      return derivedKey;
-    } else {
-      return null;
-    }
-  }
-  
-  export async function encryptAesKey(aesKey, derivedKey) {
-    const exportedAesKey = await window.crypto.subtle.exportKey('raw', aesKey);
-    const encryptedAesKey = await window.crypto.subtle.encrypt(
-      {
-        name: 'AES-CBC',
-        iv: window.crypto.getRandomValues(new Uint8Array(16))
-      },
-      derivedKey,
-      exportedAesKey
-    );
-  
-    const encryptedAesKeyString = Array.from(new Uint8Array(encryptedAesKey))
-      .map(byte => byte.toString(16).padStart(2, '0'))
-      .join('');
-  
-    return encryptedAesKeyString;
-  }
-  
-  export async function getAesKey() {
-    const derivedKey = await generateDerivedKey("titi"); // Récupère la clé dérivée
-    console.log(derivedKey);
-    const encryptedAesKeyString = localStorage.getItem('encryptedAesKey');
-    if (encryptedAesKeyString) {
-      const decryptedAesKey = await decryptWithAESKey(encryptedAesKeyString, derivedKey); // Déchiffre la clé AES chiffrée
-      const encoder = new TextEncoder();
-      const encodedDecryptedAesKey = encoder.encode(decryptedAesKey);
-      const decryptedAesKeyBytes =  new Uint8Array(encodedDecryptedAesKey);
-      const aesKey = await window.crypto.subtle.importKey(
-        'raw',
-        decryptedAesKeyBytes, //tableau de bytes
-        {
-          name: 'AES-CBC'
-        },
-        false,
-        ['encrypt', 'decrypt']
-      );
-      console.log('AesKey :');
-      console.log(aesKey);
-      return aesKey;
-    } else {
-      return null;
-    }
-  }
-  
   export async function decryptAesKey(encryptedAesKey, derivedKey) {
     if (encryptedAesKey) {
     
@@ -225,9 +186,75 @@ export async function storeAesKeyEncrypted(aesKey) {
     }
   }
   
+  export async function getAesKey() {
+    const derivedKey = await generateDerivedKey("titi"); // Récupère la clé dérivée
+    console.log(derivedKey);
+    const encryptedAesKeyString = localStorage.getItem('encryptedAesKey');
+    if (encryptedAesKeyString) {
+      const decryptedAesKey = await decryptBytesWithAes(encryptedAesKeyString, derivedKey); // Déchiffre la clé AES chiffrée
+      console.log(decryptedAesKey);
+      const aesKey = await window.crypto.subtle.importKey(
+        'raw',
+        decryptedAesKey, //tableau de bytes
+        {
+          name: 'AES-CBC'
+        },
+        false,
+        ['encrypt', 'decrypt']
+      );
+      console.log('AesKey :');
+      console.log(aesKey);
+      return aesKey;
+    } else {
+      return null;
+    }
+  }
+  
+
+  
    
   
+  export async function storeLogs(url, id, psw) {
+    try{
+      var aesKey = await getAesKey();
+      console.log(aesKey);
+      const encryptedPsw = await encryptWithAES(psw, aesKey);
+      localStorage.setItem("url_"+url, id + ';' + encryptedPsw);
+      addNewUrl(url);
+    }
+    catch(error){
+      console.log(error);
+    }
   
+  }
+  
+  export function storeUrlList(urlList) {
+    // Convertir la liste en une chaîne de caractères
+    const urlListString = urlList.join(';');
+  
+    // Stocker la chaîne de caractères dans le localStorage
+    localStorage.setItem('urlList', urlListString);
+  }
+  
+  export function getUrlList() {
+    const urlListString = localStorage.getItem('urlList');
+  
+    if (urlListString) {
+      const urlList = urlListString.split(';');
+      return urlList;
+    } else {
+      return [];
+    }
+  }
+  
+  export function addNewUrl(url) {
+    var urlList = getUrlList();
+    // Vérification si l'URL existe déjà dans la liste
+    if (!urlList.includes("url_"+url)) {
+      urlList.push(url);
+      storeUrlList(urlList);
+    }
+  }
   
   
   
