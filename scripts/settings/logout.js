6ade955df531f8c8569a51bb198bd936257e5e07
LAOUAR Augustin <a.laouar@Viveris.lan>
2024-07-04 17:00:06 +0200

    logout.js

--- scripts/settings/logout.js
+++ scripts/settings/logout.js
@@ -4,6 +4,6 @@ import { logout } from '../login_tools.js';
 document.addEventListener('DOMContentLoaded', function() {
     const logOutButton = document.getElementById('log-out');
     logOutButton.addEventListener("click", async function(){
-      logout(true);
+      logout();
     });
 });
