1ba3c274b772b4e1a5a2bbddd9fb68e615b2fc54
augustin <augustinlaouar@hotmail.com>
2024-08-03 17:21:50 +0200

    licence and author message

--- scripts/manager/handlers/import.js
+++ scripts/manager/handlers/import.js
@@ -1,3 +1,20 @@
+/*
+ * Author: Augustin Laouar
+ * Project Repository: https://github.com/augustin-laouar/Jack
+ * License: GNU General Public License v3.0
+ * 
+ * This project is licensed under the GNU General Public License v3.0.
+ * You may obtain a copy of the License at
+ * 
+ *     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import * as storage from '../../tools/storage.js';
 import * as error from '../../exception/error.js';
 import * as crypto from '../../tools/crypto.js';
